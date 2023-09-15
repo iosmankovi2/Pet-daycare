609dfdbe403183d0b1a04ff352ff4c3bcb2d00a0
Ilhana <iosmankovi2@etf.unsa.ba>
2023-09-15 19:40:53 +0200

    Dodan test u klasu VrticTest

--- src/test/java/ba/unsa/etf/rpr/VrticTest.java
+++ src/test/java/ba/unsa/etf/rpr/VrticTest.java
@@ -1,4 +1,28 @@
 package ba.unsa.etf.rpr;
 
+import org.junit.Test;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 public class VrticTest {
+
+    /**
+     * Testira dodavanje rezervacije u vrtić.
+     */
+    @Test
+
+    public void testDodajRezervaciju() {
+        Vrtic vrtic = new Vrtic("Pet daycare");
+        VlasnikKucnogLjubimca vlasnik = new VlasnikKucnogLjubimca("Hana","Hanić");
+        KucniLjubimac ljubimac = new KucniLjubimac("Fren","Mačka");
+        Rezervacija rezervacija = new Rezervacija(vlasnik,ljubimac,new Date(),new Date());
+
+        vrtic.dodajRezervaciju(rezervacija);
+
+        List<Rezervacija> rezervacije = vrtic.getRezervacije();
+        assertTrue(rezervacije.contains(rezervacija));
+    }
+
 }
