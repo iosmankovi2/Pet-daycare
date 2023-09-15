55b8e2ae518656884b411376388d9deff9cfbe3f
Ilhana <iosmankovi2@etf.unsa.ba>
2023-09-15 16:02:44 +0200

    First class KucniLjubimac is added

---
+++ src/main/java/ba/unsa/etf/rpr/KucniLjubimac.java
@@ -0,0 +1,54 @@
+package ba.unsa.etf.rpr;
+
+/**
+ * Klasa predstavlja kućnog ljubimca.
+ */
+
+public class KucniLjubimac {
+
+    private String ime;
+    private String vrsta;
+
+    /**
+     * Konstruktor za kućnog ljubimca.
+     *
+     * @param ime Ime kućnog ljubimca
+     * @param vrsta Vrsta kućnog ljubimca
+     */
+
+    public KucniLjubimac(String ime, String vrsta){
+        this.ime = ime;
+        this.vrsta = vrsta;
+    }
+
+    /**
+     * Getter za atribut ime
+     * @return ime;
+     */
+
+    public String getIme(){
+        return ime;
+    }
+
+    /**
+     * Getter za atribut vrsta
+     * @return vrsta
+     */
+
+    public String getVrsta(){
+        return vrsta;
+    }
+
+    /*
+     * Setteri za atribute ime i vrsta
+     */
+
+    public void setIme(String ime){
+        this.ime = ime;
+    }
+
+    public void setVrsta(String vrsta){
+        this.vrsta = vrsta;
+    }
+
+}
