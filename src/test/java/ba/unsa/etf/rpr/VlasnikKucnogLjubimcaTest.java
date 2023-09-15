bb1d649a0b2a5c86220ef16953cad2868d4e4b2b
Ilhana <iosmankovi2@etf.unsa.ba>
2023-09-15 19:32:19 +0200

    Dodana test u klasu VlasnikKucnogLjubimcaTestKucniLjubimacTest

--- src/test/java/ba/unsa/etf/rpr/VlasnikKucnogLjubimcaTest.java
+++ src/test/java/ba/unsa/etf/rpr/VlasnikKucnogLjubimcaTest.java
@@ -1,4 +1,26 @@
 package ba.unsa.etf.rpr;
 
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 public class VlasnikKucnogLjubimcaTest {
+
+    /**
+     * Testira dodavanje kućnog ljubimca vlasniku.
+     */
+    @Test
+
+    public void testDodajLjubimca(){
+        VlasnikKucnogLjubimca vlasnik = new VlasnikKucnogLjubimca("Hana","Hanić");
+        KucniLjubimac ljubimac = new KucniLjubimac("Fren","Mačka");
+
+        vlasnik.dodajLjubimca(ljubimac);
+
+        List<KucniLjubimac> ljubimci = vlasnik.getLjubimci();
+        assertTrue(ljubimci.contains(ljubimac));
+    }
+
 }
