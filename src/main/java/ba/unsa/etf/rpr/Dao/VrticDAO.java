45ed1ae42221fbb124bc6c254ed174862910a135
Ilhana <iosmankovi2@etf.unsa.ba>
2023-09-16 15:19:42 +0200

    17

--- src/main/java/ba/unsa/etf/rpr/Dao/VrticDAO.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/VrticDAO.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.Dao;
 
+import ba.unsa.etf.rpr.Rezervacija;
 import ba.unsa.etf.rpr.Vrtic;
 
 import java.util.List;
@@ -7,6 +8,11 @@ import java.util.List;
 public interface VrticDAO {
     Vrtic getByNazuv(String naziv);
     List<Vrtic> getAll();
+    void insert(Vrtic vrtic);
+    void update(Vrtic vrtic);
+    void delete(String naziv);
+    List<Rezervacija> getRezervacija(Vrtic vrtic);
+
 
 
 }
