a583f4af27ea27553df55648046d420d876fb7e3
Ilhana <iosmankovi2@etf.unsa.ba>
2023-09-16 15:59:10 +0200

    new dao implementation2

--- src/main/java/ba/unsa/etf/rpr/DAOImplements/RezervacijaDAOImplements.java
+++ src/main/java/ba/unsa/etf/rpr/DAOImplements/RezervacijaDAOImplements.java
@@ -21,5 +21,40 @@ public class RezervacijaDAOImplements implements RezervacijaDAO {
         return null; //Ako nije pronađena rezervacija sa datim ID-om
     }
 
+    @Override
+
+    public List<Rezervacija> getAll(){
+        //Vraća sve rezervacije iz liste
+        return rezervacije;
+    }
+
+    @Override
+
+    public void insert(Rezervacija rezervacija){
+        //Generiše novi ID za rezervaciju
+
+        int newId = generateNewId();
+        rezervacija.setId(newId);
+        rezervacija.add(rezervacija);
+    }
+
+    private int generateNewId() {
+    }
+
+    @Override
+
+    public void update(Rezervacija rezervacija){
+        //Pronalazi postojeću rezervaciju prema ID-u i zamjenjuje je novim podacima
+
+        for(int i = 0; i < rezervacije.size(); i++){
+            if(rezervacija.get(i).getId() == rezervacija.getId())
+            {
+                rezervacije.set(i,rezervacija);
+                return;
+            }
+        }
+    }
+
+
 
 }
