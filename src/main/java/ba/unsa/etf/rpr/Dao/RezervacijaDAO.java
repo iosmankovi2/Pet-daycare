b474ddf68fb603601a69eea14bfda31daab476a2
Ilhana <iosmankovi2@etf.unsa.ba>
2023-09-16 15:08:49 +0200

    3

--- src/main/java/ba/unsa/etf/rpr/Dao/RezervacijaDAO.java
+++ src/main/java/ba/unsa/etf/rpr/Dao/RezervacijaDAO.java
@@ -2,7 +2,12 @@ package ba.unsa.etf.rpr.Dao;
 
 import ba.unsa.etf.rpr.Rezervacija;
 
+import java.util.List;
+
 public interface RezervacijaDAO {
 
     Rezervacija getById(int id);
+    List<Rezervacija> getAll();
+    void insert(Rezervacija rezervacija);
+
 }
