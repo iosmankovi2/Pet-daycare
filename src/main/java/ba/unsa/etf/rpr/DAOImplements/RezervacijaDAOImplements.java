bacda6a68beec5a80b9a297ab4b3c9852b858b2e
Ilhana <iosmankovi2@etf.unsa.ba>
2023-09-16 15:49:54 +0200

    new dao implementation

--- src/main/java/ba/unsa/etf/rpr/DAOImplements/RezervacijaDAOImplements.java
+++ src/main/java/ba/unsa/etf/rpr/DAOImplements/RezervacijaDAOImplements.java
@@ -1,4 +1,16 @@
 package ba.unsa.etf.rpr.DAOImplements;
 
-public class RezervacijaDAOImplements {
+import ba.unsa.etf.rpr.Dao.RezervacijaDAO;
+import ba.unsa.etf.rpr.Rezervacija;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RezervacijaDAOImplements implements RezervacijaDAO {
+    private List<Rezervacija> rezervacije = new ArrayList<>();
+
+    @Override
+
+
+
 }
