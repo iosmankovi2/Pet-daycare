984ffdb2663a2079791584f505b6206c187ec32b
Ilhana <iosmankovi2@etf.unsa.ba>
2023-09-16 16:14:49 +0200

    new implements VlasnikKucnogLjubimcaDAOImplements1

--- src/main/java/ba/unsa/etf/rpr/DAOImplements/VlasnikKucnogLjubimcaDAOImplements.java
+++ src/main/java/ba/unsa/etf/rpr/DAOImplements/VlasnikKucnogLjubimcaDAOImplements.java
@@ -1,8 +1,29 @@
 package ba.unsa.etf.rpr.DAOImplements;
 
 import ba.unsa.etf.rpr.Dao.VlasnikKucnogLjubimcaDAO;
+import ba.unsa.etf.rpr.VlasnikKucnogLjubimca;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VlasnikKucnogLjubimcaDAOImplements implements VlasnikKucnogLjubimcaDAO {
+
+private List<VlasnikKucnogLjubimca> vlasnici = new ArrayList<>();
+
+@Override
+
+    public VlasnikKucnogLjubimca getById(int id){
+    //  Pronalazak vlasnika po Id-u iz liste
+
+    for(VlasnikKucnogLjubimca vlasnik: vlasnici){
+        if(vlasnik.getId() == id){
+            return vlasnik;
+        }
+    }
+    return null; //Ako nije pronađen vlasnik sa datim ID-om
+}
+
+    @Override
 
 
 
