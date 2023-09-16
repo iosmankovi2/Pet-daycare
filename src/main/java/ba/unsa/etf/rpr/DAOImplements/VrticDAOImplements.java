4dce4b196e52f4d782d7487ac767e526fc09dda4
Ilhana <iosmankovi2@etf.unsa.ba>
2023-09-16 16:36:59 +0200

    Implements VrticDAOImplements

--- src/main/java/ba/unsa/etf/rpr/DAOImplements/VrticDAOImplements.java
+++ src/main/java/ba/unsa/etf/rpr/DAOImplements/VrticDAOImplements.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.DAOImplements;
 
-public class VrticDAOImplements {
+import ba.unsa.etf.rpr.Dao.VrticDAO;
+import ba.unsa.etf.rpr.Vrtic;
+import jdk.dynalink.linker.LinkerServices;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VrticDAOImplements implements VrticDAO {
+    private List<Vrtic> vrtici = new ArrayList<>();
+
+    @Override
+
 }
